4b94d8faad11eb90c1ef072f7a88267024bacf47
kams <135052987+kamsmota@users.noreply.github.com>
2024-12-14 17:09:29 -0300

    Update adm.py

--- loja_informatica/models/adm.py
+++ loja_informatica/models/adm.py
@@ -1,6 +1,5 @@
 from config import db
 from flask import jsonify
-from models.colaboradores import Colaboradores, ponto_registro
 
 class Administradores(db.Model):
     __tablename__ = 'admins'
@@ -8,7 +7,8 @@ class Administradores(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String, nullable=False)
     data_nasci = db.Column(db.Date, nullable=False)
-    usera = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    user = db.Column(db.String(50), nullable=False, unique=True)
     senha = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
@@ -18,26 +18,8 @@ class Administradores(db.Model):
         return {"id": self.id, "nome": self.nome, "user": self.user, "senha": self.senha}
 
 
-adms = Administradores.query.all()
-
-
-# cadastrar um novo colaborador
-def cadastrar_colaborador(nome, data_nasci, user, senha, salario=0.0):
-    novo_colaborador = Colaboradores( #classe importada de colaboradores.py
-        nome=nome,
-        data_nasci=data_nasci,
-        user=user,
-        senha=senha,
-        salario=salario,
-    )
-    if not all([nome, data_nasci, user, senha]):
-        return jsonify({"error": "Preencha todos os campos obrigatórios."}), 400
-    db.session.add(novo_colaborador)
-    db.session.commit()
-    return jsonify({"message": f"Colaborador {nome} cadastrado com sucesso!"})
-
-# registrar salário de um colaborador
 def atualizar_salario(colaborador_id, salario=None):
+    from models.colaboradores import Colaboradores
     #att o salário de um colaborador
     colaborador = Colaboradores.query.get(colaborador_id)
     if not colaborador:
@@ -50,6 +32,22 @@ def atualizar_salario(colaborador_id, salario=None):
     return jsonify({"message": f"Dados do colaborador {colaborador.nome} atualizados com sucesso!"})
 
 def listar_todos_pontos():
-    if not ponto_registro:
+    from models.ponto import PontoRegistro
+    if not PontoRegistro:
         return jsonify({"message": "Nenhum registrado ainda."}), 404
-    return jsonify(ponto_registro)
+    return jsonify(PontoRegistro)
+
+def cadastrar_colaborador(nome, data_nasci, user, senha, salario=0.0):
+        from models.colaboradores import Colaboradores
+        novo_colaborador = Colaboradores( #classe importada de colaboradores.py
+        nome=nome,
+        data_nasci=data_nasci,
+        user=user,
+        senha=senha,
+        salario=salario,
+        )
+        if not all([nome, data_nasci, user, senha]):
+            return jsonify({"error": "Preencha todos os campos obrigatórios."}), 400
+        db.session.add(novo_colaborador)
+        db.session.commit()
+        return jsonify({"message": f"Colaborador {nome} cadastrado com sucesso!"})
