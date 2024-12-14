af03b6c067ecf23036687f03574c8aa91fc1de99
kams <135052987+kamsmota@users.noreply.github.com>
2024-12-14 18:40:45 -0300

    Update admRepository.py

--- loja_informatica/repository/admRepository.py
+++ loja_informatica/repository/admRepository.py
@@ -17,3 +17,33 @@ class AdministradoresRepository:
 
     def delete_adm(self, adm_id):
         return self.AdmsDao.del_adm(adm_id)
+    
+def atualizar_salario(colaborador_id, salario=None):
+    from models.colaboradores import Colaboradores
+    #att o salário de um colaborador
+    colaborador = Colaboradores.query.get(colaborador_id)
+    if not colaborador:
+        return jsonify({"error": "Colaborador não registrado."}), 404
+
+    if salario is not None:
+        colaborador.salario = salario
+
+    db.session.commit()
+    return jsonify({"message": f"Dados do colaborador {colaborador.nome} atualizados com sucesso!"})
+
+
+
+def cadastrar_colaborador(nome, data_nasci, user, senha, salario=0.0):
+        from models.colaboradores import Colaboradores
+        novo_colaborador = Colaboradores( #classe importada de colaboradores.py
+        nome=nome,
+        data_nasci=data_nasci,
+        user=user,
+        senha=senha,
+        salario=salario,
+        )
+        if not all([nome, data_nasci, user, senha]):
+            return jsonify({"error": "Preencha todos os campos obrigatórios."}), 400
+        db.session.add(novo_colaborador)
+        db.session.commit()
+        return jsonify({"message": f"Colaborador {nome} cadastrado com sucesso!"})
