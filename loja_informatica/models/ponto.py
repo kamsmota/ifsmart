84bc71063d9c491628fa0a6f8ad955a21f92e4e3
kams <135052987+kamsmota@users.noreply.github.com>
2024-12-14 17:10:01 -0300

    Create ponto.py

---
+++ loja_informatica/models/ponto.py
@@ -0,0 +1,13 @@
+from config import db
+from datetime import datetime
+
+
+class PontoRegistro:
+    __tablename__ = 'registro de ponto'
+    colaborador_id = db.Column(db.Integer, primary_key=True, nullable = True)
+    data = db.Column(db.DateTime, nullable=False)
+    periodo = db.Column(db.String(10), nullable=False)
+     #funcao p pegar o tempo atual e o criar como objeto time
+    
+def get_current_time():
+    return datetime.now().time()
