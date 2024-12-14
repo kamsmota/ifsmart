fb4fd87cc0088924bbd370f6478ad307884ba785
kams <135052987+kamsmota@users.noreply.github.com>
2024-12-14 18:38:28 -0300

    Update colabDAO.py

--- loja_informatica/dao/colabDAO.py
+++ loja_informatica/dao/colabDAO.py
@@ -4,24 +4,24 @@ from config import db
 class ColaboradoresDAO:
     @staticmethod
     def get_colaborador(id):
-        return colaboradores.query.get(id)
+        return Colaboradores.query.get(id)
 
     @staticmethod
     def get_all_colaboradores():
-        return colaboradores.query.all()
+        return Colaboradores.query.all()
 
     @staticmethod
-    def add_colaborador(name, email):
-        colaborador = colaborador(name=name, email=email)
+    def add_colaborador(nome, email):
+        colaborador = colaborador(nome=nome, email=email)
         db.session.add(colaborador)
         db.session.commit()
         return colaborador
 
     @staticmethod
-    def att_colaborador(id, name, email):
+    def att_colaborador(id, nome, email):
         colaborador = ColaboradoresDAO.get_colaborador(id)
         if colaborador:
-            colaborador.name = name
+            colaborador.nome = nome
             colaborador.email = email
             db.session.commit()
         return colaborador
