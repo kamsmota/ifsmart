6b6f077d9b7ef8c52025d3196924e845b9f9cc8f
kams <135052987+kamsmota@users.noreply.github.com>
2024-12-14 18:45:25 -0300

    Update app.py

--- loja_informatica/app.py
+++ loja_informatica/app.py
@@ -5,39 +5,41 @@ from controllers.auth_controller import auth_bp
 from controllers.cart_controller import cart_bp
 from controllers.item_controller import item_controller
 from controllers.adm_controller import admController
+from controllers.colabs_controller import colabsController
 from flask_login import LoginManager
 from models.user import User  
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Inicialize o SQLAlchemy com o app
+
 db.init_app(app)
 
-# Inicializa o LoginManager
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# Função para carregar o usuário
+
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))  # Retorna o usuário a partir do ID
+    return User.query.get(int(user_id))
 
-# Registra os blueprints
+
 app.register_blueprint(cart_bp)
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(product_bp, url_prefix='/products')
 app.register_blueprint(item_controller, url_prefix='/items')
 app.register_blueprint(admController, url_prefix='/adm')
+app.register_blueprint(colabsController, url_prefix='/colabs')
 
 
 @app.route('/')
 def home():
-    return redirect(url_for('product_bp.index'))  # Direciona para o controller de produtos
+    return redirect(url_for('product_bp.index'))
 
 if __name__ == "__main__":
     with app.app_context():
         db.metadata.clear()
         db.create_all()
-        db.create_all()  # Cria as tabelas no banco de dados
+        db.create_all()
     app.run(debug=True)
