3d0cda384b5bbdba9997de42d6a4f9b1fa118a37
kams <135052987+kamsmota@users.noreply.github.com>
2024-12-14 12:43:19 -0300

    Update adm.py

--- loja_informatica/models/adm.py
+++ loja_informatica/models/adm.py
@@ -3,7 +3,6 @@ from flask import jsonify
 from datetime import datetime
 from models.colaboradores import Colaboradores
 
-# Modelo Administradores
 class Administradores(db.Model):
     __tablename__ = 'admins'
 
@@ -13,13 +12,12 @@ class Administradores(db.Model):
     user = db.Column(db.String(50), nullable=False, unique=True)
     senha = db.Column(db.Text, nullable=False)
 
-# Funções relacionadas aos administradores
 
-# Função para listar todos os pontos de colaboradores
+#  listar todos os pontos de colaboradores
 def listar_todos_pontos(pontos):
     return jsonify({"pontos": pontos})
 
-# Função para cadastrar um novo colaborador
+# cadastrar um novo colaborador
 def cadastrar_colaborador(nome, data_nasci, user, senha, salario=0.0):
     novo_colaborador = Colaboradores(
         nome=nome,
@@ -32,18 +30,15 @@ def cadastrar_colaborador(nome, data_nasci, user, senha, salario=0.0):
     db.session.commit()
     return jsonify({"message": f"Colaborador {nome} cadastrado com sucesso!"})
 
-# Função para registrar salário ou férias de um colaborador
-def atualizar_salario_ferias(colaborador_id, salario=None, ferias=None):
-    """Atualiza o salário ou status de férias de um colaborador."""
+# registrar salário
+def atualizar_salario_ferias(colaborador_id, salario=None):
+    # att salário ou status de férias de um colaborador
     colaborador = Colaboradores.query.get(colaborador_id)
     if not colaborador:
         return jsonify({"error": "Colaborador não encontrado."}), 404
 
     if salario is not None:
         colaborador.salario = salario
-
-    if ferias is not None:
-        colaborador.ferias = ferias
 
     db.session.commit()
     return jsonify({"message": f"Dados do colaborador {colaborador.nome} atualizados com sucesso!"})
