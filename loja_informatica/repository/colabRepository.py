5ccdec727b004da66d90d43938e54b0027893bcd
kams <135052987+kamsmota@users.noreply.github.com>
2024-12-14 18:41:03 -0300

    Update colabRepository.py

--- loja_informatica/repository/colabRepository.py
+++ loja_informatica/repository/colabRepository.py
@@ -3,6 +3,7 @@ from flask import *
 from datetime import datetime
 from models.colaboradores import *
 from models.ponto import *
+from repository.pontoRepository import *
 
 class ColaboradoresRepository:
     def __init__(self) -> None:
@@ -22,7 +23,7 @@ class ColaboradoresRepository:
         return self.ColabsDao.del_colaborador(colaborador_id)
 
     #funcao p realmente bater ponto
-    def ponto():
+def ponto():
         from models.ponto import PontoRegistro
         data = request.json
         colaborador_id = data.get('colaborador_id')
@@ -55,11 +56,11 @@ class ColaboradoresRepository:
         return jsonify(registro)
 
     #funcao p pontos do colaborador selecionado pelo id
-    def get_ponto(colaborador_id):
-        colaborador_pontos = [registro for registro in PontoRegistro if registro['colaborador_id'] == colaborador_id]
+def get_ponto(colaborador_id):
+    colaborador_pontos = [registro for registro in PontoRegistro if registro['colaborador_id'] == colaborador_id]
     
-        if not colaborador_pontos:
-            return jsonify({"message": "Nenhum ponto registrado ainda."}), 404
+    if not colaborador_pontos:
+        return jsonify({"message": "Nenhum ponto registrado ainda."}), 404
 
-        return jsonify(colaborador_pontos)
+    return jsonify(colaborador_pontos)
 
