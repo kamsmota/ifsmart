a009e421f1af12bacdd3f11eaceb71202912ea49
FeYukie <135060199+FeYukie@users.noreply.github.com>
2024-12-15 21:53:40 -0300

    Create __init__.py

---
+++ loja_informatica/models/__init__.py
@@ -0,0 +1,6 @@
+from .adm_controller import * #nome do arquivo 
+from .auth_controller import *
+from .carrinho_controlador import *
+from .colabs_controller.py import *
+from .item_controller.py import *
+from .produto_controlador.py import *
