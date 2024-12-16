1f908362da5cc8aed1426ffa0f3498b5fcf9f15b
FeYukie <135060199+FeYukie@users.noreply.github.com>
2024-12-15 22:03:17 -0300

    Create __init__.py

---
+++ loja_informatica/repository/__init__.py
@@ -0,0 +1,6 @@
+from admRepository import *
+from cart_repository import *
+from colabRepository import *
+from item_repository import *
+from pontoRepository import *
+from user_repository import *
