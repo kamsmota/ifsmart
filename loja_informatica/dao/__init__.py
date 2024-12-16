dea9eb58dfba45252ac88ec9b8bb88def2768bfa
FeYukie <135060199+FeYukie@users.noreply.github.com>
2024-12-15 21:59:36 -0300

    Create __init__.py

---
+++ loja_informatica/dao/__init__.py
@@ -0,0 +1,8 @@
+from .adm import * #nome do arquivo 
+from .cart import *
+from .cart_item import *
+from .colaboradores import *
+from .item import *
+from .ponto import *
+from .product import *
+from .user import *
